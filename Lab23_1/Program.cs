c81f7a413998bd5d5e3b928bd0eb43de71e3e135
Антон Еремеев <kzanton_81@mail.ru>
2022-01-09 22:06:13 +0300

    Добавьте файлы проекта.

---
+++ Lab23_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab23_1
+{
+    class Program
+    {         
+        static async Task<decimal> Factorial(int n)
+        {
+            return await Task.Run<decimal>(() =>
+            {
+                decimal result = 1;
+                for (int i = 1; i <= n; i++) result *= i;
+                return result;
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите число, для которого будем считать факториал:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + Factorial(n).Result);
+            Console.ReadLine();
+        }
+    }
+}
